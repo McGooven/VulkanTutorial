30e6c17ec8ce2bb0e8883fa6ea8d0e2890cd8b99
Diego Sandon Rabuco <diego.esr.contacto@gmail.com>
2024-06-09 19:45:58 -0400

    configuring the pipeline stages and behaviour.

--- src/lve_pipeline.cpp
+++ src/lve_pipeline.cpp
@@ -55,6 +55,62 @@ namespace lve {
 
     PipelineConfigInfo LvePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height){
         PipelineConfigInfo configInfo{};
+        configInfo.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        // this says that every 3 vertices the line are gourp together into a separate triangle
+        configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        configInfo.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+        /**
+         * Viewport describe the transformation between the pipeline's output
+         * and the target image (usually fitting the image to this boundles: squshing or stretching it).
+         * 
+         * Viewport actually tells the pipeline how to transform the gl position values
+         * to the output images.
+         * 
+         * minDepth and maxDepth linearly transform the z component of gl position.
+        */
+        configInfo.viewport.x = 0.0f;
+        configInfo.viewport.y = 0.0f;
+        configInfo.viewport.width = static_cast<float>(width);
+        configInfo.viewport.height = static_cast<float>(height);
+        configInfo.viewport.minDepth = 0.0f;
+        configInfo.viewport.maxDepth = 1.0f;
+        
+        /**
+         * scissor tells the pipeline output how to transform the image too, like viewport, but instead
+         * scissor cut the image to fit it.
+        */
+        configInfo.scissor.offset = {0, 0};
+        configInfo.scissor.extent = {width, height};
+
+        /**
+         * See https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/VkPipelineViewportStateCreateInfo.html
+         * for more info about these properties
+        */
+        configInfo.viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        configInfo.viewportInfo.viewportCount = 1;
+        configInfo.viewportInfo.pViewports = &configInfo.viewport;
+        configInfo.viewportInfo.scissorCount = 1;
+        configInfo.viewportInfo.pScissors = &configInfo.scissor;
+
+
+        /**
+         * Raterization Stage 
+         * 
+         * this stage breaks up the geometry into fragments for each pixel or triangle overlap.
+        */
+        configInfo.rasterizationInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        configInfo.rasterizationInfo.depthClampEnable = VK_FALSE;
+        configInfo.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
+        configInfo.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
+        configInfo.rasterizationInfo.lineWidth = 1.0f;
+        configInfo.rasterizationInfo.cullMode = VK_CULL_MODE_NONE;
+        configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        configInfo.rasterizationInfo.depthBiasEnable = VK_FALSE;
+        configInfo.rasterizationInfo.depthBiasConstantFactor = 0.0f;  // Optional
+        configInfo.rasterizationInfo.depthBiasClamp = 0.0f;           // Optional
+        configInfo.rasterizationInfo.depthBiasSlopeFactor = 0.0f;     // Optional
+
         return configInfo;
     }
 }
